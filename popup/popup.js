ea6441e798605b24ef76f03c93da465dc8fa53f9
Nakiame <max.iv.slep@gmail.com>
2022-03-14 21:47:34 +0200

    Significant changes

--- popup/popup.js
+++ popup/popup.js
@@ -1,25 +1,28 @@
-//*********************[ Добавить action для формы из активной вкладки ]*********************//
+//*********************[ Add action for form from active tab ][ Delete checkCustom.php ]*********************//
 chrome.tabs.query({active:true, currentWindow: true},function(tabsArray) {
-    // Текущий URL
+// Current URL
     let tab = tabsArray[0];
     let tabUrl = tab.url;
-
-    // Парсинг по '/'
+// Parsing by '/'
     let domainName = tabUrl.split('/');
-
-    // Добавляем action к форме
+// Adding an action to the form
     let f = document.getElementById('formCustom');
     f.action = domainName[0] + '//' + domainName[2] + '/checkCustom.php';
+
+    let buttonDelete = document.getElementById("deleteCheckCustom");
+    buttonDelete.href = domainName[0] + '//' + domainName[2] + '/checkCustom.php?delFile=Y';
+
+    let getListCustom = document.getElementById("getListCustom");
+    getListCustom.href = domainName[0] + '//' + domainName[2] + '/checkCustom.php?getListCustom=Y';
+
 });
 
-
-//*********************[ Обновить активную вкладку после отправки формы ]*********************//
+//*********************[ Refresh active tab after form submit ]*********************//
 let buttonForm = document.getElementById("buttonForm");
 buttonForm.addEventListener("click", async () => {
-    // Получить ID текущей вкладки
+// Get current tab ID
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    // Выполнить скрипт в tabID
+// Execute script in tabID
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
         function: reloadTab,
@@ -29,24 +32,20 @@ function reloadTab() {
     window.location.reload();
 }
 
-//*********************[ Закрыть расширение ]*********************//
+//*********************[ Close extension ]*********************//
 let buttonClose = document.getElementById("buttonClose");
 buttonClose.addEventListener("click", async () => {
     window.close();
 });
 
-//*********************[ Загрузить checkCustom.php ]*********************//
+//*********************[ Upload checkCustom.php ]*********************//
 let buttonUpload = document.getElementById("uploadCheckCustom");
-
 chrome.tabs.query({active:true, currentWindow: true},function(tabsArray) {
     let tab = tabsArray[0];
     let tabUrl = tab.url;
-
     let domainName = tabUrl.split('/');
-
     buttonUpload.href = domainName[0] + '//' + domainName[2] + '/bitrix/admin/php_command_line.php';
 });
-
 buttonUpload.addEventListener("click", async () => {
     let text = "$referenceCheckCustom = file('https://raw.githubusercontent.com/nakiamegit/Support24/main/checkCustom.php');\n" +
         "$tempCheckCustom = fopen($_SERVER['DOCUMENT_ROOT'] . \"/checkCustom.php\", \"a+\");\n" +
@@ -54,25 +53,71 @@ buttonUpload.addEventListener("click", async () => {
         "    fwrite($tempCheckCustom, $line);\n" +
         "}\n" +
         "fclose($tempCheckCustom);";
-
         let area = document.createElement('textarea');
-
         document.body.appendChild(area);
         area.value = text;
         area.select();
         document.execCommand("copy");
         document.body.removeChild(area);
-
 });
 
-//*********************[ Удалить checkCustom.php ]*********************//
-let buttonDelete = document.getElementById("deleteCheckCustom");
+//*********************[ Radio ]*********************//
+divRadio1 = document.getElementById('radioCustomItem-1'); divRadio2 = document.getElementById('radioCustomItem-2');
+elRadio1 = document.getElementById('fidRadioCustom-1'); elRadio2 = document.getElementById('fidRadioCustom-2');
+divRadio1.onclick = function() {
+    divRadio1.hidden = true; elRadio1.checked = false;
+    divRadio2.hidden = false; elRadio2.checked = true;
+}
+divRadio2.onclick = function() {
+    divRadio2.hidden = true; elRadio2.checked = false;
+    divRadio1.hidden = false; elRadio1.checked = true;
+}
 
-chrome.tabs.query({active:true, currentWindow: true},function(tabsArray) {
-    let tab = tabsArray[0];
-    let tabUrl = tab.url;
+//*********************[ Checking file exists ]*********************//
+divNotifyIsFileCustom = document.getElementById('notifyIsFileCustom');
+function checkIsFileCustom(){
+    chrome.tabs.query({active:true, currentWindow: true},function(tabsArray) {
+        let tab = tabsArray[0];
+        let tabUrl = tab.url;
+        let domainNameSplit = tabUrl.split('/');
+        let domainName = domainNameSplit[0] + '//' + domainNameSplit[2] + '/checkCustom.php';
 
-    let domainName = tabUrl.split('/');
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', domainName,  true)
+        xhr.send()
 
-    buttonDelete.href = domainName[0] + '//' + domainName[2] + '/checkCustom.php?delFile=Y';
-});
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState != 4) { return }
+            if (xhr.status === 200) {
+                divNotifyIsFileCustom.style.display = 'block';
+            } else {
+                divNotifyIsFileCustom.style.display = 'none';
+            }
+        }
+    });
+}
+setInterval(checkIsFileCustom, 1000);
+
+//*********************[ Checking disabled custom ]*********************//
+divNotifyDisabledCustom = document.getElementById('notifyDisabledCustom');
+function checkDisabledCustom(){
+    chrome.tabs.query({active:true, currentWindow: true},function(tabsArray) {
+        let tab = tabsArray[0];
+        let tabUrl = tab.url;
+        let domainNameSplit = tabUrl.split('/');
+        let domainName = domainNameSplit[0] + '//' + domainNameSplit[2] + '/checkCustom.php?checkDisabledCustom=Y';
+
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', domainName,  true)
+        xhr.send()
+
+        xhr.onreadystatechange = function() {
+            if (xhr.status === 200) {
+                divNotifyDisabledCustom.style.display = 'block';
+            } else {
+                divNotifyDisabledCustom.style.display = 'none';
+            }
+        }
+    });
+}
+setInterval(checkDisabledCustom, 1000);
